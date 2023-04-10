da84cd0c93c6d41cb860ba14a34bc4b71bd1a72c
Kyle Lin <b40885@gmail.com>
2023-04-10 14:53:19 +0800

    paddlehub example

---
+++ Synsized.py
@@ -0,0 +1,14 @@
+import paddlehub as hub
+
+model = hub.Module(name='ge2e_fastspeech2_pwgan', output_dir='./', speaker_audio='./data/zh.wav')  # 指定目标音色音频文件
+# model = hub.Module(name='ge2e_fastspeech2_pwgan')  # 指定目标音色音频文件
+texts = [
+    '语音的表现形式在未来将变得越来越重要$',
+    '今天的天气怎么样$',
+    '你今天過得好嗎?'  ]
+wavs = model.generate(texts, use_gpu=False)
+
+for text, wav in zip(texts, wavs):
+    print('='*30)
+    print(f'Text: {text}')
+    print(f'Wav: {wav}')
